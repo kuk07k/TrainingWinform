0086b730b1246c79397c3a5de8e4e18b73ace9f2
BangYongHyuk <kuk07k@naver.com>
2020-06-15 18:32:13 +0900

    Winform

--- WinFormApp/ClockApp/Form1.Designer.cs
+++ WinFormApp/ClockApp/Form1.Designer.cs
@@ -63,6 +63,9 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Clock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- WinFormApp/ClockApp/Form1.cs
+++ WinFormApp/ClockApp/Form1.cs
@@ -22,5 +22,34 @@ namespace ClockApp
             DateTime now = DateTime.Now;
             label1.Text = now.ToString("HH:mm:ss");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 로드시 발생");
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 활성화시 발생");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Form 클로즈시 발생");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("진짜 닫을래?", "경고",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) // (질문 , 질문창, 질문에 대한 대답, 아이콘종류)
+            {
+                e.Cancel = false; //취소를 안시켜서 종료
+            }
+            else
+            {
+                e.Cancel = true; //취소시켜서 종료 안됨
+            }
+        }
+
     }
 }
